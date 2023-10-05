e768b82a62fc9d5f0c22ca799b08db01c951440b
yanshay <yanivshaya@gmail.com>
2023-10-05 13:48:41 +0300

    switched to download footprints from my fork of infused-kim footprints

--- ergogen_src/footprints/download_fps.sh
+++ ergogen_src/footprints/download_fps.sh
@@ -7,9 +7,9 @@ curl https://raw.githubusercontent.com/yanshay/ergogen-stuff/main/footprints/pad
 curl https://raw.githubusercontent.com/yanshay/ergogen-stuff/main/footprints/router.js -o router.js
 curl https://raw.githubusercontent.com/yanshay/ergogen-stuff/main/footprints/switch_reset_panasonic.js -o switch_reset_panasonic.js
 
-curl https://raw.githubusercontent.com/infused-kim/kb_ergogen_fp/main/conn_molex_pico_ezmate_1x02.js -o conn_molex_pico_ezmate_1x02.js
-curl https://raw.githubusercontent.com/infused-kim/kb_ergogen_fp/main/icon_bat.js -o icon_bat.js
-curl https://raw.githubusercontent.com/infused-kim/kb_ergogen_fp/main/nice_view.js -o nice_view.js
-curl https://raw.githubusercontent.com/infused-kim/kb_ergogen_fp/main/switch_power.js -o switch_power.js
-curl https://raw.githubusercontent.com/infused-kim/kb_ergogen_fp/main/text.js -o text.js
+curl https://raw.githubusercontent.com/yanshay/kb_ergogen_fp/main/conn_molex_pico_ezmate_1x02.js -o conn_molex_pico_ezmate_1x02.js
+curl https://raw.githubusercontent.com/yanshay/kb_ergogen_fp/main/icon_bat.js -o icon_bat.js
+curl https://raw.githubusercontent.com/yanshay/kb_ergogen_fp/main/nice_view.js -o nice_view.js
+curl https://raw.githubusercontent.com/yanshay/kb_ergogen_fp/main/switch_power.js -o switch_power.js
+curl https://raw.githubusercontent.com/yanshay/kb_ergogen_fp/main/text.js -o text.js
 # curl https://raw.githubusercontent.com/infused-kim/kb_ergogen_fp/main/trackpoint_mount.js -o trackpoint_mount.js
